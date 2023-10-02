e9952ae89ee4458cf4e45994f8e53f126930aee7
Crec0 <83436716+Crec0@users.noreply.github.com>
2023-10-01 17:42:15 -0700

    reformat

--- src/database.ts
+++ src/database.ts
@@ -1,37 +1,38 @@
-import {integer, sqliteTable, text} from 'drizzle-orm/sqlite-core';
-import {sql} from 'drizzle-orm';
-import {BetterSQLite3Database, drizzle} from 'drizzle-orm/better-sqlite3';
+import { integer, sqliteTable, text } from 'drizzle-orm/sqlite-core';
+import { sql } from 'drizzle-orm';
+import { BetterSQLite3Database, drizzle } from 'drizzle-orm/better-sqlite3';
 import Database from 'better-sqlite3';
 
-export const db: BetterSQLite3Database = drizzle(new Database('./tags.db'), {logger: true});
 
-db.run(sql`
+export const db: BetterSQLite3Database = drizzle( new Database( './tags.db' ), { logger: true } );
+
+db.run( sql`
     CREATE TABLE IF NOT EXISTS tags
     (
-        tag_id           INTEGER PRIMARY KEY AUTOINCREMENT,
-        time_created     INTEGER NOT NULL DEFAULT (UNIXEPOCH()),
-        tag_name         TEXT    NOT NULL,
-        content          TEXT    NOT NULL,
-        author_username  TEXT    NOT NULL,
-        author_user_id   TEXT    NOT NULL,
-        guild_id         TEXT    NOT NULL
+        tag_id          INTEGER PRIMARY KEY AUTOINCREMENT,
+        time_created    INTEGER NOT NULL DEFAULT (UNIXEPOCH()),
+        tag_name        TEXT    NOT NULL,
+        content         TEXT    NOT NULL,
+        author_username TEXT    NOT NULL,
+        author_user_id  TEXT    NOT NULL,
+        guild_id        TEXT    NOT NULL
     );
-`);
+` );
 
-export const tagsTable = sqliteTable('tags', {
-    tagID: integer('tag_id').primaryKey().notNull(),
-    timeCreated: integer('time_created', {mode: 'timestamp'}).notNull().default(sql`(UNIXEPOCH())`),
-    tagName: text('tag_name').notNull(),
-    content: text('content').notNull(),
-    authorUsername: text('author_username').notNull(),
-    authorUserID: text('author_user_id').notNull(),
-    guildID: text('guild_id').notNull()
-});
+export const tagsTable = sqliteTable( 'tags', {
+    tagID: integer( 'tag_id' ).primaryKey().notNull(),
+    timeCreated: integer( 'time_created', { mode: 'timestamp' } ).notNull().default( sql`(UNIXEPOCH())` ),
+    tagName: text( 'tag_name' ).notNull(),
+    content: text( 'content' ).notNull(),
+    authorUsername: text( 'author_username' ).notNull(),
+    authorUserID: text( 'author_user_id' ).notNull(),
+    guildID: text( 'guild_id' ).notNull(),
+} );
 
 export type TagsInsertType = typeof tagsTable.$inferInsert
 export type TagsSelectType = typeof tagsTable.$inferSelect
 
-db.run(sql`
+db.run( sql`
     CREATE TABLE IF NOT EXISTS attachments
     (
         tag_id INTEGER NOT NULL,
@@ -40,14 +41,14 @@ db.run(sql`
         type   TEXT,
         FOREIGN KEY (tag_id) REFERENCES tags (tag_id)
     );
-`);
+` );
 
-export const attachmentTable = sqliteTable('attachments', {
-    tagID: integer('tag_id').notNull().references(() => tagsTable.tagID),
-    name: text('name').notNull(),
-    url: text('url').notNull(),
-    type: text('type'),
-});
+export const attachmentTable = sqliteTable( 'attachments', {
+    tagID: integer( 'tag_id' ).notNull().references( () => tagsTable.tagID ),
+    name: text( 'name' ).notNull(),
+    url: text( 'url' ).notNull(),
+    type: text( 'type' ),
+} );
 
 export type AttachmentInsertType = typeof attachmentTable.$inferInsert
 export type AttachmentSelectType = typeof attachmentTable.$inferSelect
--- src/index.ts
+++ src/index.ts
@@ -1,50 +1,51 @@
 import dotenv from 'dotenv';
-import {ActivityType, Client, GatewayIntentBits, Guild, Interaction} from 'discord.js';
-import {commands, handleAutocomplete, handleChatCommand, handleMessageContextMenuCommand} from './commands.js';
+import { ActivityType, Client, GatewayIntentBits, Guild, Interaction } from 'discord.js';
+import { commands, handleAutocomplete, handleChatCommand, handleMessageContextMenuCommand } from './commands.js';
+
 
 dotenv.config();
 
-export const client = new Client({
+export const client = new Client( {
     intents: [
         GatewayIntentBits.Guilds,
         GatewayIntentBits.GuildMessages,
     ],
-    allowedMentions: {parse: []},
+    allowedMentions: { parse: [] },
     presence: {
-        activities: [{name: '/tag', type: ActivityType.Listening}],
+        activities: [ { name: '/tag', type: ActivityType.Listening } ],
     },
-});
+} );
 
-client.on('interactionCreate', async (interaction: Interaction) => {
-    if (interaction.isAutocomplete()) {
-        await handleAutocomplete(interaction).catch(console.log);
+client.on( 'interactionCreate', async ( interaction: Interaction ) => {
+    if ( interaction.isAutocomplete() ) {
+        await handleAutocomplete( interaction ).catch( console.log );
 
-    } else if (interaction.isMessageContextMenuCommand()) {
-        await handleMessageContextMenuCommand(interaction).catch(error => {
-            console.log(error);
-            if (interaction.deferred || interaction.replied) {
-                interaction.editReply({content: 'There was an error while executing this command'});
+    } else if ( interaction.isMessageContextMenuCommand() ) {
+        await handleMessageContextMenuCommand( interaction ).catch( error => {
+            console.log( error );
+            if ( interaction.deferred || interaction.replied ) {
+                interaction.editReply( { content: 'There was an error while executing this command' } );
             } else {
-                interaction.reply({content: 'There was an error while executing this command', ephemeral: true});
+                interaction.reply( { content: 'There was an error while executing this command', ephemeral: true } );
             }
-        });
+        } );
 
-    } else if (interaction.isChatInputCommand()) {
-        await handleChatCommand(interaction).catch(error => {
-            console.log(error);
-            if (interaction.deferred) {
-                interaction.editReply({content: 'There was an error while executing this command'});
+    } else if ( interaction.isChatInputCommand() ) {
+        await handleChatCommand( interaction ).catch( error => {
+            console.log( error );
+            if ( interaction.deferred ) {
+                interaction.editReply( { content: 'There was an error while executing this command' } );
             } else {
-                interaction.reply({content: 'There was an error while executing this command', ephemeral: true});
+                interaction.reply( { content: 'There was an error while executing this command', ephemeral: true } );
             }
-        });
+        } );
 
     }
-});
+} );
 
-client.once('ready', async () => {
-    console.log(`${client.user!.tag} is online!`);
-    await client.application!.commands.set(commands);
-});
+client.once( 'ready', async () => {
+    console.log( `${ client.user!.tag } is online!` );
+    await client.application!.commands.set( commands );
+} );
 
-await client.login(process.env.DISCORD_TOKEN);
+await client.login( process.env.DISCORD_TOKEN );
