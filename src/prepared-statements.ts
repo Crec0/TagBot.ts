c239f53df15fd99028bb3873b2c1e4fa1dffaf6e
Crec0 <83436716+Crec0@users.noreply.github.com>
2023-10-01 17:38:32 -0700

    Adjust prepared statements and reformat

--- src/prepared-statements.ts
+++ src/prepared-statements.ts
@@ -1,22 +1,22 @@
-import {asc, eq, sql} from 'drizzle-orm';
-import {attachmentTable, db, tagsTable} from './database.js';
+import { eq, sql } from 'drizzle-orm';
+import { attachmentTable, db, tagsTable } from './database.js';
+
 
 export const getTagPreparedStatement = db
     .select()
-    .from(tagsTable)
-    .where(eq(tagsTable.tagName, sql.placeholder('query')))
-    .orderBy(asc(tagsTable.tagName))
-    .limit(1)
-    .prepare(false);
+    .from( tagsTable )
+    .where( eq( tagsTable.tagID, sql.placeholder( 'tag_id' ) ) )
+    .limit( 1 )
+    .prepare( false );
 
 export const listTagsPreparedStatement = db
-    .select({name: tagsTable.tagName})
-    .from(tagsTable)
-    .where(eq(tagsTable.guildID, sql.placeholder('guildId')))
-    .prepare(false);
+    .select( { name: tagsTable.tagName, id: tagsTable.tagID } )
+    .from( tagsTable )
+    .where( eq( tagsTable.guildID, sql.placeholder( 'guild_id' ) ) )
+    .prepare( false );
 
 export const getAttachmentsPreparedStatement = db
-    .select({url: attachmentTable.url})
-    .from(attachmentTable)
-    .where(eq(attachmentTable.tagID, sql.placeholder('tagId')))
-    .prepare(false);
+    .select()
+    .from( attachmentTable )
+    .where( eq( attachmentTable.tagID, sql.placeholder( 'tag_id' ) ) )
+    .prepare( false );
